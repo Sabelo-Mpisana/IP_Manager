18a2beea94e88129e9c3a0702e0ae2ed313494a5
Bbusan0857 <bbubbuh16b@gmail.com>
2025-04-22 10:07:55 +0200

    Add project files.

---
+++ Data/DbsContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace IP_Manager.Data
+{
+    public class DbsContext:DbContext
+    {
+        public DbsContext(DbContextOptions<DbsContext> options) : base(options) 
+        {
+            
+        }
+
+
+    }
+}
